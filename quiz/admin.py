7ed667a37d30be17a523795c9e68bacc5fd3003e
royaleagle-dev <royaleagle.dev@gmail.com>
2019-08-17 23:32:18 +0100

    2.0 beta admin-site fixes

--- quiz/admin.py
+++ quiz/admin.py
@@ -4,8 +4,30 @@ from django.contrib import admin
 
 from quiz.models import Question, Choice, MyUser, Category, Subject
 
-admin.site.register(Question)
-admin.site.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ['question', 'choice', 'flag']
+    list_display_links = ('choice',)
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 0
+    max_num = 4
+    can_delete = False
+    min_num =  1
+    
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'subject']
+    empty_value_display = "{{null}}"
+    #fields = ('title', ('category', 'subject'))
+    fieldsets = (('Question',{'fields': ('title',)}),
+     ('Options', {
+         'fields':(('category', 'subject'),)
+     })
+    )
+    inlines = [ChoiceInline,]
+    
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice, ChoiceAdmin)
 admin.site.register(MyUser)
 admin.site.register(Category)
 admin.site.register(Subject)
