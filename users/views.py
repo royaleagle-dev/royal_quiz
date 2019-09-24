0548f92d5cb08bba5b7d00fd538d35f048896abe
royaleagle-dev <royaleagle.dev@gmail.com>
2019-09-24 15:55:07 +0100

    version 2.0 stable

--- users/views.py
+++ users/views.py
@@ -19,9 +19,7 @@ def approve(request, pk):
     subject = 'New Question'
     content = "Question: {0},\n Options: {1}, {2}, {3}, {4} \n Answer: {5}".format(pQuestion.question, pQuestion.option1, pQuestion.option2, pQuestion.option3, pQuestion.option4, pQuestion.answer)
     to = ['ayotundeokunubi73@gmail.com']
-    from_email = 'royaleagle.dev@gmail.com'
-    
-    msg = EmailMessage(subject, content,from_email, to)
+    msg = EmailMessage(subject, content, to)
     msg.send()
     pQuestion.delete()
     return redirect('users:approveQuestion')
